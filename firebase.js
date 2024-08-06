d352c87bcdea892d08c0aa61db44b09498f9cc29
Andrew Presman <alpresma@buffalo.edu>
2024-08-06 18:28:40 -0400

    fb stuff

--- firebase.js
+++ firebase.js
@@ -1,16 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-require("dotenv").config();
-
 
 const firebaseConfig = {
-    apiKey: process.env.fireapiKey,
-    authDomain: process.env.fireauthDomain,
-    projectId: process.env.fireprojectId,
-    storageBucket: process.env.firestorageBucket,
-    messagingSenderId: process.env.firemessagingSenderId,
-    appId: process.env.fireappId,
-    measurementId: process.env.firemeasurementId
+    apiKey: "AIzaSyB3E2Q2ZYkMZTI3IEEhZv4Hew4q-Ol6OPA",
+    authDomain: "ai-projects-35650.firebaseapp.com",
+    projectId: "ai-projects-35650",
+    storageBucket: "ai-projects-35650.appspot.com",
+    messagingSenderId: "240442172964",
+    appId: "1:240442172964:web:7a533346803e5842e79ab1",
+    measurementId: "G-WQM1G63Y3F"
   };
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
